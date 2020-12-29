3b898f1b5344e18aa84455c4dabd67cda5e65857
PalalaeStefan301 <palalaestefan301@yahoo.com>
2020-12-29 23:13:29 +0200

    server/client added

--- create_db.sql
+++ create_db.sql
@@ -1,7 +1,13 @@
 
 CREATE TABLE IF NOT EXISTS regions (
 	region_id INTEGER PRIMARY KEY,
-	region_name TEXT NOT NULL	
+	region_name TEXT NOT NULL,
+    temperature INTEGER NOT NULL,
+    humidity INTEGER NOT NULL,
+    uv_index INTEGER NOT NULL,
+    sunrise TEXT NOT NULL,
+    sunset TEXT NOT NULL,
+    wind INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS cities (
@@ -23,11 +29,11 @@ CREATE TABLE IF NOT EXISTS cities (
 DELETE FROM regions;
 DELETE FROM cities;
 
-INSERT INTO regions(region_name) VALUES("California");
-INSERT INTO regions(region_name) VALUES("Florida");
-INSERT INTO regions(region_name) VALUES("Pennsylvania");
-INSERT INTO regions(region_name) VALUES("Illinois");
-INSERT INTO regions(region_name) VALUES("Texas");
+INSERT INTO regions(region_name,temperature,humidity,uv_index,sunrise,sunset,wind) VALUES("California",63,47,3,"6:58 AM","4:52 PM", 10);
+INSERT INTO regions(region_name,temperature,humidity,uv_index,sunrise,sunset,wind) VALUES("Florida",63,47,3,"6:58 AM","4:52 PM", 10);
+INSERT INTO regions(region_name,temperature,humidity,uv_index,sunrise,sunset,wind) VALUES("Pennsylvania",63,47,3,"6:58 AM","4:52 PM", 10);
+INSERT INTO regions(region_name,temperature,humidity,uv_index,sunrise,sunset,wind) VALUES("Illinois",63,47,3,"6:58 AM","4:52 PM", 10);
+INSERT INTO regions(region_name,temperature,humidity,uv_index,sunrise,sunset,wind) VALUES("Texas",63,47,3,"6:58 AM","4:52 PM", 10);
 
 INSERT INTO cities(city_name,region_id,temperature,humidity,uv_index,sunrise,sunset,wind)
 VALUES ("Los Angeles",1,63,47,3,"6:58 AM","4:52 PM", 10);
